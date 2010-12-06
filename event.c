9826a86681c555866f7e80681fc05eb6de21641c
Lilian Maurel <lilian@maurel.biz>
2010-12-07 00:25:26 +0100

    update event file

--- event.c
+++ event.c
@@ -5,10 +5,29 @@
 #include "dtab.h"
 #include "event.h"
 
-typedef struct event_t
+typedef enum event_kind_t
+{
+    EVENT_KIND_FD,
+    EVENT_KIND_TIMER
+} event_kind_t;
+
+typedef struct event_fd_t
 {
     event_f *fnc;
     void    *data;
+} event_fd_t;
+
+typedef struct event_timer_t
+{
+} event_timer_t;
+
+typedef struct event_t
+{
+    event_kind_t        kind;
+    union {
+        event_fd_t      event_fd;
+        event_timer_t   event_timer;
+    };
 } event_t;
 
 DTAB(pollfd, struct pollfd);
@@ -44,10 +63,20 @@ void event_register_fd(int fd, event_f *fnc, short event, void *data)
     pevt = dtab_add(event,  events, NULL);
 
     pfd->fd         = fd; 
+
     pfd->events     = event;
 
-    pevt->fnc       = fnc;
-    pevt->data      = data;
+    pevt->kind               = EVENT_KIND_FD;
+    pevt->event_fd.fnc       = fnc;
+    pevt->event_fd.data      = data;
+}
+
+void event_register_timer(int fd, event_f *fnc, short event, void *data)
+{
+    fd =fd;
+    fnc= fnc;
+    event= event;
+    data=data;
 }
 
 void event_exit(void)
@@ -66,7 +95,7 @@ void event_loop(void)
     assert(fds);
     assert(events);
 
-    while(noexist)
+    while(noexit)
     {
         v = poll(fds->data, fds->size, -1);
         if(v == -1)
@@ -84,9 +113,9 @@ void event_loop(void)
             pevt = dtab_get(event, events, i); 
             if(evt)
             {
-                if(pevt->fnc)
+                if(pevt->event_fd.fnc)
                 {
-                    pevt->fnc(i, pfd, pevt->data);
+                    pevt->event_fd.fnc(i, pfd, pevt->event_fd.data);
                 }
                 v--;
             }
