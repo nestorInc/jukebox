f0c84fe5561b5e5e6e8ef1c32ba179e952418b9b
Lilian Maurel <lilian@maurel.biz>
2010-12-15 19:00:05 +0100

    add dlist

---
+++ dlist.h
@@ -0,0 +1,49 @@
+#ifndef __DLIST_H__
+#define __DLIST_H__
+
+typedef struct dlist_t {
+    struct dlist_t *prev;
+    struct dlist_t *next;
+} dlist_t;
+
+
+static inline void dlist_init(dlist_t *e)
+{
+    e->prev = e;
+    e->next = e;
+}
+
+static inline void dlist_add_next(dlist_t *h, dlist_t *e)
+{
+    e->next       = h->next;
+    e->prev       = h;
+    e->prev->next = e;
+    e->next->prev = e;
+}
+
+static inline void dlist_add_prev(dlist_t *h, dlist_t *e)
+{
+    e->next       = h;
+    e->prev       = h->prev;
+    e->prev->next = e;
+    e->next->prev = e;
+}
+
+static inline int dlist_is_empty(dlist_t *h)
+{
+    return h->next == h->prev;
+}
+
+static inline struct dlist_t * dlist_del(struct dlist_t *e)
+{
+    e->prev->next = e->next;
+    e->next->prev = e->prev;
+
+    return e;
+}
+
+
+#define dlist_for_each(h)                                               \
+  for(dlist_t *cur = (h)->next; cur != (h); cur = cur->next)
+
+#endif /* __DLIST_H__ */
