410612313833673d415a27b5d0335c0d82a6ddb0
Lilian Maurel <lilian@maurel.biz>
2011-07-18 23:32:59 +0200

    clean up repo

--- common.h
+++
@@ -1,7 +0,0 @@
-#ifndef __COMMON_H__
-#define __COMMON_H__
-
-#define MIN(a, b) ((a) < (b)) ? (a) : (b)
-#define MAX(a, b) ((a) > (b)) ? (a) : (b)
-
-#endif /* __COMMON_H__ */
--- mem-circ-chunk.h
+++
@@ -1,5 +0,0 @@
-#ifndef __MEM_CIRC_CHUNK__
-#define __MEM_CIRC_CHUNK__
-
-
-#endif /* __MEM_CIRC_CHUNK__ */
