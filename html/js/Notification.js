34104daeb58b31a8872d5d8379d78aa3a62e943e
beuiot <beuiot@gmail.com>
2011-11-13 18:56:15 +0100

    Notification.js comments

--- html/js/Notification.js
+++ html/js/Notification.js
@@ -1,10 +1,13 @@
+// Array of notifications that are currently on screen.
 var activeNotifications = new Array();
 
+// This has to be loaded first. It starts the update loop and show a welcome / test notification.
 function initNotifications () {
     setTimeout(updateNotifications, 1000);
     showNotification(2,"Client loaded.");
 }
 
+// Main update loop. It checks whether notifications should be removed.
 function updateNotifications () {
     for (var i = 0; i < activeNotifications.length; i++) {
 	var notification = activeNotifications[i];
@@ -17,6 +20,8 @@ function updateNotifications () {
     setTimeout(updateNotifications, 1000);
 }
 
+
+// Find a notification from its DOM id and if found, removes it.
 function removeNotificationFromId(id) {
     for (var i = 0; i < activeNotifications.length; i++) {
 	var notification = activeNotifications[i];
@@ -27,6 +32,7 @@ function removeNotificationFromId(id) {
     }
 }
 
+// Remove the given notification.
 function removeNotificationFromIndex(index) {
     var notification = activeNotifications[index];
     notification.remove();
@@ -37,14 +43,23 @@ function showNotification(level, message) {
     activeNotifications.push(new Notification(level, message));
 }
 
+// Main class containing notification stuff.
+// It is capable of appearing, updating its content, and disappearing.
 function Notification (level, message) {
+    // Notification infos
     this.level = level;
     this.message = message;
+
+    // Default display time is 8 seconds.
     this.remainingTime = 8;
+    
+    // The id has to be unique. Generating one from time and current array length.
     this.id = activeNotifications.length + 1 + (last_time = new Date().getTime() / 1000);
     
     
     this.style = "notification_wrapper_";
+
+    // Assign correct style and time according to given level.
     switch(level) {
     case 1:
 	this.style += "debug";
@@ -71,23 +86,32 @@ function Notification (level, message) {
 	break;
     }
     
+    // Insert notification wrapper in the page
     $('notifications').insert('<div class="notification_wrapper" id="notification' + this.id + '"><div class="' + this.style + '" id="notification_content'+this.id+'"></div></div>');
+
+    // Update content of notification
     this.update();
+    
+    // Hide notification, then make it appear
     $('notification'+this.id).hide();
-    Effect.SlideDown('notification'+this.id, { duration: 0.6 , afterFinish: function(effect) {
-	Event.observe(effect.element, 'click', function(event) {
-	    var element_id = $(Event.element(event)).up('.notification_wrapper').id.substring(12);
-	    removeNotificationFromId(element_id);
-	});	
-    }});
+    Effect.SlideDown('notification'+this.id,
+		     { duration: 0.6 , afterFinish: function(effect) {
+			 // Once the notification appeared, make the click on its wrapper close it.
+			 Event.observe(effect.element, 'click', function(event) {
+			     var element_id = $(Event.element(event)).up('.notification_wrapper').id.substring(12);
+			     removeNotificationFromId(element_id);
+			 });
+		     }});
     return true;
 }
 
+// Updates the content of a notification
 Notification.prototype.update = function()
 {
     $('notification_content'+this.id).update('<p>' + this.message + ' </p>');
 };
 
+// Make a notification disappear
 Notification.prototype.remove = function()
 {
     Effect.SlideUp('notification'+this.id, {duration: 0.4, afterFinish: function(effect) { 
