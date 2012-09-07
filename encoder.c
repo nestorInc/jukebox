29078a71a94f954deb050a3ee39c62ad3370e8a7
Lilian Maurel <lilian@maurel.biz>
2012-09-07 12:34:55 +0200

    encoder: scan all 30s and cache inode,atime

--- encoder.c
+++ encoder.c
@@ -10,7 +10,7 @@
 #include "vector.h"
 #include "mp3.h"
 
-typedef struct thread_cmd {
+typedef struct thread_cmd_t {
     unsigned int         cmd;
     void                *data;
 } thread_cmd_t;
@@ -209,7 +209,7 @@ void encode_th(void *data)
         dup2(pipedesc[0], 0);
         argv[0] = "lame";
         argv[1] = "-";
-        argv[2] = enc->dst; // Dst
+        argv[2] = enc->dst;
         argv[3] = "-r";
         argv[4] = "-b";
         argv[5] = "192";
@@ -232,78 +232,118 @@ void encode_th(void *data)
     free(data);
 }
 
+typedef struct inode_cache_t {
+    ino_t  ino;
+    time_t atime;
+} inode_cache_t;
+
+VECTOR_T(inode_cache, inode_cache_t);
+
+int inode_cache_insert(vector_inode_cache_t *cache, ino_t ino, time_t atime)
+{
+    int                 i;
+    inode_cache_t       entry;
+
+    for(i = 0; i < cache->len; ++i) {
+        inode_cache_t *pentry = &cache->data[i];
+
+        if(pentry->ino == ino) {
+            if(pentry->atime == atime)
+                return -1;
+            // update
+            pentry->atime = atime;
+            return 0;
+        }
+    }
+    
+    entry.ino   = ino;
+    entry.atime = atime;
+
+    vector_inode_cache_push(cache, &entry);
+
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
-    int                 nb_thread = 4;
-    jobs_queue_t        jq;
-    int                 cmd;
-    DIR                *dp; 
-    struct dirent      *dirp;
+    int                         nb_thread = 4;
+    jobs_queue_t                jq;
+    int                         cmd;
+    DIR                        *dp; 
+    struct dirent              *dirp;
 
-    int                 srcdir_len   = strlen(argv[1]);
-    int                 dstdir_len   = strlen(argv[2]);
+    int                         srcdir_len   = strlen(argv[1]);
+    int                         dstdir_len   = strlen(argv[2]);
 
+    // Replace by hash tab
+    vector_inode_cache_t        inode_cache;
+    time_t                      cur_time;
+
+    int                         scan_time    = 30; // 30s
 
     argc = argc;
+
+    vector_inode_cache_init(&inode_cache);
 
     jobs_queue_init(&jq, nb_thread);
     cmd = jobs_queue_func_add(&jq, encode_th);
 
-    dp = opendir(argv[1]);
-    if(dp == NULL)
-        return 1;
+    while(1) {
+        cur_time = time(NULL);
+        printf("new scan\n");
+        dp = opendir(argv[1]);
+        if(dp == NULL)
+            return 1;
 
-    while ((dirp = readdir(dp)) != NULL) {
-        encode_file_t *data;
-        int            len = strlen(dirp->d_name);
-        char          *pos;
+        while ((dirp = readdir(dp)) != NULL) {
+            encode_file_t *data;
+            int            len = strlen(dirp->d_name);
+            char          *pos;
 
-        if((len == 1 && memcmp(dirp->d_name, "." , 1) == 0) ||
-           (len == 2 && memcmp(dirp->d_name, "..", 2) == 0))
-            continue;
+            if((len == 1 && memcmp(dirp->d_name, "." , 1) == 0) ||
+               (len == 2 && memcmp(dirp->d_name, "..", 2) == 0))
+                continue;
 
-        data = malloc(sizeof(encode_file_t) +
-                      len + srcdir_len + 1 + 1 +
-                      len + dstdir_len + 1 + 1);
+            data = malloc(sizeof(encode_file_t) +
+                          len + srcdir_len + 1 + 1 +
+                          len + dstdir_len + 1 + 1);
 
-        pos = data->data;
+            pos = data->data;
 
-        data->src = pos;
-        memcpy(pos, argv[1], srcdir_len);
-        pos += srcdir_len;
-        *pos = '/';
-        pos++;
-        memcpy(pos, dirp->d_name, len + 1);
-        pos += len + 1;
+            data->src = pos;
+            memcpy(pos, argv[1], srcdir_len);
+            pos += srcdir_len;
+            *pos = '/';
+            pos++;
+            memcpy(pos, dirp->d_name, len + 1);
+            pos += len + 1;
 
-        data->dst = pos;
-        memcpy(pos, argv[2], dstdir_len);
-        pos += dstdir_len;
-        *pos = '/';
-        pos++;
-        memcpy(pos, dirp->d_name, len + 1);
-        pos += len + 1;
+            data->dst = pos;
+            memcpy(pos, argv[2], dstdir_len);
+            pos += dstdir_len;
+            *pos = '/';
+            pos++;
+            memcpy(pos, dirp->d_name, len + 1);
+            pos += len + 1;
 
-        if(dirp->d_type == DT_UNKNOWN) {
             struct stat buf;
 
             stat(data->src, &buf);            
 
-            if(!S_ISREG(buf.st_mode)) {
+            if(!S_ISREG(buf.st_mode) || buf.st_atime + (scan_time * 2) > cur_time ||
+               inode_cache_insert(&inode_cache, buf.st_ino, buf.st_atime) == -1) {
                 /* Check if is dir and scan it */
                 free(data);
                 continue;
             }
-        } else {
-            if(dirp->d_type != DT_REG) {
-                free(data);
-                continue;
-            }
-        }
 
-        jobs_queue_add(&jq, cmd, data);        
+            printf("add %s\n", data->src);
+
+            jobs_queue_add(&jq, cmd, data);        
+        }
+        closedir(dp);
+        sleep(scan_time);
     }
-    closedir(dp);
     
     jobs_queue_wait(&jq);
 
