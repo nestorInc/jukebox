112750ab82b2f5046c7a31eb12695687cfe3f2e7
TiTi <anthibug@gmail.com>
2012-10-20 18:26:37 +0200

    Fix typo (seen in a notification)

--- json_api.rb
+++ json_api.rb
@@ -159,7 +159,7 @@ class JsonManager < HttpNode
       ch.set_plugin(req["plugin_name"]);
     else
       error("Unknown action #{req["name"]}", true, $error_file);
-      JsonManager.add_message(resp, MSG_LVL_ERROR, nil, "unknown action #{req["name"]}");
+      JsonManager.add_message(resp, MSG_LVL_ERROR, nil, "Unknown action #{req["name"]}");
     end
   end
 
