bc43999420d199dd6d32e042e7428bd6a6d4de4a
beuiot <beuiot@gmail.com>
2013-03-04 17:15:02 +0100

    Restructuring main player UI - added title bar and put relevant infos in it

--- html_src/js/skin/hype.js
+++ html_src/js/skin/hype.js
@@ -13,33 +13,63 @@ Jukebox.UI.skins["hype"] =
 		player:
 '<div class="#{root} #{root}-theme-#{theme}">\
 <div class="#{root}-main">\
+<div class="#{root}-main-titlebar">\
+<table class="#{root}-main-titlebar">\
+<td class="#{root}-main-titlebar-title">\
+Home made jukebox over streaming\
+</td>\
+<td class="#{root}-listening">\
+	<div class="#{root}-listening-ico"></div>\
+	<div class="#{root}-listening-count">#{listenersCount}</div>\
+</td>\
+<td class="#{root}-main-titlebar-refresh-button">\
+	<input type="button" class="#{root}-refresh-button" value="" />\
+</td>\
+<td class="#{root}-main-titlebar-autorefresh-checkbox">\
+	<input type="checkbox" name="#{root}-autorefresh" class="#{root}-autorefresh" checked="checked" value="autorefresh" />\
+</td>\
+<td class="#{root}-activity"></td>\
+</table>\
+</div>\
+<table class="#{root}-main-current-song">\
+<tr>\
+	<td class="#{root}-main-current-song-cover">\
+		<div class="#{root}-main-current-song-cover-wrapper">\
+			<img class="#{root}-song-cover #{root}-main-current-song-cover" />\
+		</div>\
+	</td>\
+	<td class="#{root}-main-current-song-infos">\
+		<table class="#{root}-main-current-song-infos">\
+			<tr>\
+				<td>\
+					#{currentSong}\
+				</td>\
+			</tr>\
+			<tr>\
+				<td class="#{root}-progressbar-container">\
+					<p class="#{root}-song-time"></p>\
+					<div class="#{root}-progressbar-wrapper">\
+						<div class="#{root}-progressbar"></div>\
+					</div>\
+				</td>\
+			</tr>\
+		</table>\
+	</td>\
+</tr>\
+</table>\
 <table class="#{root}-main">\
 <tr>\
-	<td class="#{root}-activity"></td>\
 	<td class="#{root}-controls">\
 		<a href="javascript:;" class="#{root}-previous-button"></a>\
 		<a href="javascript:;" class="#{root}-stream-play" title="#{play}"></a>\
 		<a href="javascript:;" class="#{root}-stream-stop" title="#{stop}"></a>\
 		<a href="javascript:;" class="#{root}-next-button"></a>\
 	</td>\
-	<td class="#{root}-progressbar-container">\
-		<p class="#{root}-song-time"></p>\
-		<div class="#{root}-progressbar-wrapper">\
-			<div class="#{root}-progressbar"></div>\
-		</div>\
-	</td>\
-	<td>\
-		#{currentSong}\
-	</td>\
 	<td class="#{root}-volume">\
 		<div class="#{root}-volume-ico"></div>\
 		<div class="#{root}-volume-slider">\
 			<div class="#{root}-volume-handle"></div>\
 		</div>\
-	</td>\
-	<td class="#{root}-listening">\
-		<div class="#{root}-listening-ico"></div>\
-		<div class="#{root}-listening-count">#{listenersCount}</div>\
 	</td>\
 	<td class="#{root}-expand-collapse">\
 		<a href="javascript:;" class="#{root}-expand-button"></a>\
