cddc021fd21e4a181e2fa6d3b57dd57f542c441e
trashman <olivier.kleparski@gmail.com>
2013-08-09 23:19:34 +0200

    start to implements user_space

--- db.rb
+++ db.rb
@@ -3,6 +3,7 @@
 require 'sqlite3'
 
 require 'display.rb'
+require 'user_space.rb'
 
 class Song
   attr_accessor :mid
@@ -96,93 +97,134 @@ class Library
 
   def initialize()
     @db = SQLite3::Database.new("jukebox.db")
-    @db.results_as_hash = true 
+    @db.results_as_hash = true
     # Activate the use of foreign keys contraints for sqlite 3
-    @db.execute( "PRAGMA foreign_keys = ON;");
-    @db.execute( "create table if not exists library (
+    sql = <<SQL
+    PRAGMA foreign_keys = ON;
+    create table if not exists library (
                        mid INTEGER PRIMARY KEY,
                        src TEXT, dst TEXT,
                        title TEXT, artist TEXT, album TEXT, years INTEGER UNSIGNED NULL,
                        track INTEGER UNSIGNED NULL, trackNb INTEGER UNSIGNED NULL, genre INTEGER UNSIGNED NULL,
-                       status INTEGER, frames TEXT, bitrate INTEGER, duration INTEGER);" );
-    @db.execute( "create table if not exists users (
+                       status INTEGER, frames TEXT, bitrate INTEGER, duration INTEGER);
+    create table if not exists users (
                        uid INTEGER PRIMARY KEY,
                        nickname TEXT UNIQUE,
                        hash TEXT,
-                       validated INTEGER UNSIGNED, 
-                       creation INTEGER);" );
-    @db.execute( "create table if not exists sessions (
+                       validated INTEGER UNSIGNED,
+                       creation INTEGER);
+    create table if not exists sessions (
                        sid TEXT PRIMARY KEY,
                        sudo_uid INTEGER UNSIGNED,
                        uid INTEGER UNSIGNED,
                        user_agent TEXT,
                        remote_ip TEXT,
                        creation INTEGER,
+                       last_connexion INTEGER,
                        validity INTEGER,
                        FOREIGN KEY(uid) REFERENCES users(uid) ON UPDATE CASCADE ON DELETE CASCADE,
-                       FOREIGN KEY(sudo_uid) REFERENCES users(uid) ON UPDATE CASCADE ON DELETE CASCADE);" );
-    @db.execute( "create table if not exists groups ( 
-                       gid INTEGER PRIMARY KEY, 
-                       label TEXT );" );
-    @db.execute( "create table if not exists groups_users (
-                       gid INTEGER UNSIGNED,
+                       FOREIGN KEY(sudo_uid) REFERENCES users(uid) ON UPDATE CASCADE ON DELETE CASCADE);
+    create table if not exists groups (
+                       gid TEXT PRIMARY KEY,
+                       label TEXT );
+    create table if not exists groups_users (
+                       gid TEXT,
                        uid INTEGER UNSIGNED,
                        PRIMARY KEY(gid, uid),
                        FOREIGN KEY(gid) REFERENCES groups(gid) ON UPDATE CASCADE ON DELETE CASCADE,
-                       FOREIGN KEY(uid) REFERENCES users(uid) ON UPDATE CASCADE ON DELETE CASCADE);" );
-
-    # A right is designed like a path
-    # The user that own's a root path is automatically the owner of sub pathes
-    @db.execute( "create table if not exists rights (
-                       rid INTEGER PRIMARY KEY,
+                       FOREIGN KEY(uid) REFERENCES users(uid) ON UPDATE CASCADE ON DELETE CASCADE);
+    create table if not exists rights (
                        right TEXT UNIQUE,
                        owner INTEGER UNSIGNED,
                        flag_owner INTEGER UNSIGNED,
                        flag_others INTEGER UNSIGNED,
-                       FOREIGN KEY(owner) REFERENCES users(uid) ON UPDATE CASCADE ON DELETE CASCADE);" );
-    @db.execute( "create table if not exists rights_groups (
-                       rid INTEGER UNSIGNED,
-                       gid INTEGER UNSIGNED,
+                       FOREIGN KEY(owner) REFERENCES users(uid) ON UPDATE CASCADE ON DELETE CASCADE);
+    create table if not exists rights_groups (
+                       right INTEGER UNSIGNED,
+                       gid TEXT,
                        flag_group INTEGER UNSIGNED,
-                       PRIMARY KEY(rid, gid),
-                       FOREIGN KEY(rid) REFERENCES rights(rid) ON UPDATE CASCADE ON DELETE CASCADE,
-                       FOREIGN KEY(gid) REFERENCES users(uid) ON UPDATE CASCADE ON DELETE CASCADE);" );
-    # Active tokens linked to a right
-    @db.execute( "create table if not exists rights_tokens (
-                       rid INTEGER UNSIGNED,
+                       PRIMARY KEY(right, gid),
+                       FOREIGN KEY(right) REFERENCES rights(right) ON UPDATE CASCADE ON DELETE CASCADE,
+                       FOREIGN KEY(gid) REFERENCES users(uid) ON UPDATE CASCADE ON DELETE CASCADE);
+    create table if not exists rights_tokens (
+                       right INTEGER UNSIGNED,
                        tid INTEGER UNSIGNED,
-                       PRIMARY KEY(tid, rid),
-                       FOREIGN KEY(rid) REFERENCES rights(rid) ON UPDATE CASCADE ON DELETE CASCADE,
-                       FOREIGN KEY(tid) REFERENCES tokens(tid) ON UPDATE CASCADE ON DELETE CASCADE);" );
-    @db.execute( "create table if not exists tokens (
+                       PRIMARY KEY(tid, right),
+                       FOREIGN KEY(right) REFERENCES rights(right) ON UPDATE CASCADE ON DELETE CASCADE,
+                       FOREIGN KEY(tid) REFERENCES tokens(tid) ON UPDATE CASCADE ON DELETE CASCADE);
+    create table if not exists tokens (
                        tid INTEGER PRIMARY KEY,
                        token TEXT UNIQUE,
-                       rid INTEGER UNSIGNED UNIQUE,
-                       activated INTEGER UNSIGNED, 
+                       right INTEGER UNSIGNED UNIQUE,
+                       activated INTEGER UNSIGNED,
                        type INTEGER UNSIGNED,
-                       FOREIGN KEY(rid) REFERENCES rights(rid)  ON UPDATE CASCADE ON DELETE CASCADE);" );
-    @db.execute( "create table if not exists sessions_tokens (
-                       tid INTEGER PRIMARY KEY);" );
+                       FOREIGN KEY(right) REFERENCES rights(right) ON UPDATE CASCADE ON DELETE CASCADE);
+    create table if not exists sessions_tokens (
+                       tid INTEGER PRIMARY KEY);
+SQL
 
-    create_new_user("guest", "guest", 1);
+    @db.execute_batch( sql );
+
+    # # Generates a random password for root
+    char_set =  [('a'..'z'),('A'..'Z'), ('0'..'9')].map{|i| i.to_a}.flatten
+    random_string = (0...50).map{ char_set[rand(char_set.length)] }.join
+
+    # # Create base users
+    # root_uid = create_new_user("root", random_string, 1);
+    # log("root password : #{random_string}");
+    # guest_uid = create_new_user("guest", "guest", 1);
+    # create_new_user("trashman", "trashing", 1);
+    # root_grp = create_new_group("root");
+
+    # #Create base rights
+    # create_new_right("/",
+    #                  1,
+    #                  Rights_Flag::OWNER | Rights_Flag::READ | Rights_Flag::WRITE | Rights_Flag::EXECUTE | Rights_Flag::CREATION | Rights_Flag::DELETE | Rights_Flag::TOKENIZE,
+    #                  Rights_Flag::READ,
+    #                  [{ "gid" => root_grp, "flag" => Rights_Flag::OWNER | Rights_Flag::READ | Rights_Flag::WRITE | Rights_Flag::EXECUTE | Rights_Flag::CREATION | Rights_Flag::DELETE | Rights_Flag::TOKENIZE}] );
+
+
+    # create_new_right("/users/",
+    #                  1,
+    #                  Rights_Flag::OWNER | Rights_Flag::READ | Rights_Flag::WRITE | Rights_Flag::EXECUTE | Rights_Flag::CREATION | Rights_Flag::DELETE | Rights_Flag::TOKENIZE,
+    #                  Rights_Flag::READ,
+    #                  [{ "gid" => root_grp, "flag" => Rights_Flag::OWNER | Rights_Flag::READ | Rights_Flag::WRITE | Rights_Flag::EXECUTE | Rights_Flag::CREATION | Rights_Flag::DELETE | Rights_Flag::TOKENIZE}] );
+
 
     req = @db.prepare("UPDATE library SET status=#{FILE_WAIT} WHERE status=#{FILE_ENCODING_PROGRESS}");
     res = req.execute!();
     req.close();
 
-    res;
-
     log("library initialized.");
+    res;
   end
 
   def create_new_user( user, pass, validated )
     bcrypt_pass = BCrypt::Password.create(pass);
     creation = Time.now();
-    req = @db.prepare("INSERT OR IGNORE INTO users (uid, nickname, hash, validated, creation) VALUES ( ?, ?, ?, ?, ? )");
-    res = req.execute!( nil, user, bcrypt_pass, 1,creation.strftime("%s"));
-    req.close();
 
+    sql = <<SQL
+     BEGIN;
+     INSERT OR IGNORE INTO users (uid, right_path, nickname, hash, validated, creation) VALUES (NULL, '/users/#{user}', '#{user}', '#{bcrypt_pass}', 1, #{creation.strftime("%s")};
+     INSERT OR REPLACE INTO rights (right, owner, flag_owner, flag_others ) 
+     SELECT 
+        '/users/#{user}', 
+        U.uid, 
+        127, 
+        0
+     FROM users as U
+     WHERE U.nickname = '#{user}';
+     INSERT OR REPLACE INTO rights_groups (right, owner, flag_owner, flag_others ) 
+     COMMIT;
+SQL
+    @db.execute_batch( sql );
+
+    @db.execute("SELECT uid FROM users WHERE nickname='#{user}' LIMIT 1") do |row|
+      return row["uid"];
+    end
+    nil;
   end
+
 
   def login( user, pass )
       req = @db.prepare("SELECT hash FROM users WHERE nickname='#{user}' LIMIT 1");
@@ -199,14 +241,17 @@ class Library
   end
 
   def invalidate_sessions( )
-    #TODO       req = @db.prepare("DELETE FROM sessions WHERE validity < ")
+    #TODO Change a flag in order to add a message to the response when the user try to access an invalidated session
+    now = (Time.now()).strftime("%s");
+    @db.execute("DELETE FROM sessions WHERE validity <= ?", now);
   end
 
   def check_session( sid, remote_ip, user_agent )
-    req = @db.prepare("SELECT U.nickname FROM sessions as S INNER JOIN users as U ON U.uid = S.uid WHERE S.sid='#{sid}' AND S.user_agent='#{user_agent}' AND S.remote_ip='#{remote_ip}'");
-    res = req.execute!();
-    req.close();
-    return res[0].at(0) if(res[0])
+    res = @db.execute("SELECT U.nickname as nick FROM sessions as S INNER JOIN users as U ON U.uid = S.uid WHERE S.sid='#{sid}' AND S.user_agent='#{user_agent}' AND remote_ip='#{remote_ip}' AND validated=1 LIMIT 1");
+    if(res[0])
+      @db.execute("UPDATE sessions SET last_connexion=#{(Time.now()).strftime("%s")}  WHERE sid='#{sid}'");
+      return res[0]["nick"]
+    end
     nil;
   end
 
@@ -215,23 +260,20 @@ class Library
     return false if(user == nil)
 
     #retrieve user uid
-    req = @db.prepare("SELECT uid FROM users WHERE nickname='#{user}'");
-    res = req.execute!();
-    req.close();
-    uid = res[0].at(0);
+    uid = nil;
+    res = @db.execute("SELECT uid FROM users WHERE nickname='#{user}'");
+    uid = res[0]["uid"] if(res[0] != nil);
 
     #retrieve sudo_user uid
     suid = uid;
     if (sudo_user != nil)
-      req = @db.prepare("SELECT uid FROM users WHERE nickname='#{sudo_user}'");
-      res = req.execute!();
-      req.close();
-      suid = res[0].at(0);
+      res = @db.execute("SELECT uid FROM users WHERE nickname='#{sudo_user}'");
+      suid = res[0]["uid"];
     end
 
     creation = (Time.now()).strftime("%s");
     #TODO constant for session validity
-    validity = (Time.now() + (24*60*60)).strftime("%s"); 
+    validity = (Time.now() + (24*60*60)).strftime("%s");
 
     hashExists = false;
     gen_hash_retry = 0;
@@ -241,19 +283,42 @@ class Library
       # generates a random string
       # http://stackoverflow.com/questions/88311/how-best-to-generate-a-random-string-in-ruby
       random_string = (0...50).map{ char_set[rand(char_set.length)] }.join
-      req = @db.prepare("SELECT count(*) FROM sessions WHERE sid='#{random_string}'");
-      res = req.execute!();
-      req.close();
-      hashExists = true if( res[0].at(0) == 0)
+      res = @db.execute("SELECT count(*) as hash_exists FROM sessions WHERE sid='#{random_string}'");
+      hashExists = true if( res[0] != nil and res[0]["hash_exists"] == 0)
       return nil if(gen_hash_retry >= 100)
     end
 
-    req = @db.prepare("INSERT INTO sessions ( sid, sudo_uid, uid, user_agent, remote_ip, creation, validity ) VALUES ('#{random_string}', '#{suid}', '#{uid}', '#{user_agent}', '#{remote_ip}', #{creation}, #{validity})");
-    req.execute!();
-    req.close();
-    
+    @db.execute("INSERT INTO sessions ( sid, sudo_uid, uid, user_agent, remote_ip, creation, last_connexion, validity ) VALUES ('#{random_string}', '#{suid}', '#{uid}', '#{user_agent}', '#{remote_ip}', #{creation}, #{creation}, #{validity})");
+
+    # Wtf why this line doesn't work ?
+    # @db.execute("INSERT INTO sessions ( sid, sudo_uid, uid, user_agent, remote_ip, creation, last_connexion, validity ) VALUES (?,?,?,?,?,?,?,?)", random_string, suid, uid, user_agent, remote_ip, creation, creation, validity);
+
     # returns the newly session hash created
     random_string;
+  end
+
+  def create_new_group( label )
+    @db.execute("INSERT INTO groups (gid, label) VALUES (?,?)", nil, label);
+    @db.last_insert_row_id;
+  end
+
+  def create_new_right( right_path, owner_id, flag_owner, flag_others, group_rights)
+    #TODO check if subrights exists
+    #TODO check user right to create a new sub right
+    @db.execute("INSERT OR IGNORE INTO rights (right, owner, flag_owner, flag_others) VALUES( '#{right_path}', '#{owner_id}', #{flag_owner}, #{flag_others})");
+
+    begin
+      @db.prepare("INSERT OR IGNORE INTO rights_groups (right, gid, flag_group) VALUES ( ?, ?, ? )") do |stmt|
+        group_rights.each { |grp|
+          stmt.bind_param(1, right_path);
+          stmt.bind_param(2, grp["gid"]);
+          stmt.bind_param(3, grp["flag"]);
+          stmt.execute();
+        }
+      end
+    rescue => e
+      #ignore statment
+    end
   end
 
   def check_token(token)
@@ -278,14 +343,14 @@ class Library
     nil;
   end
 
-  # searching methods here 
+  # searching methods here
   def get_nb_songs()
     req = @db.prepare("SELECT COUNT (*) FROM library WHERE status=#{FILE_OK}");
     res = req.execute!();
     req.close();
     res[0].at(0);
   end
-  
+
   def get_total(field, comparison, value)
     if(field && value.size > 0)
       if( "like" == comparison)
@@ -301,7 +366,7 @@ class Library
     req.close();
     res[0].at(0);
   end
-  
+
   def get_file(*mids)
     if(mids.size == 0 || mids[0] == nil)
       req = @db.prepare("SELECT * FROM library WHERE status=#{FILE_OK} ORDER BY RANDOM() LIMIT 1");
@@ -331,7 +396,7 @@ class Library
     field   = "artist" if(field != "title" && field != "album" && field != "genre");
     orderBy = "artist COLLATE NOCASE DESC, album COLLATE NOCASE DESC, track ASC, title COLLATE NOCASE DESC" if(orderBy == nil);
 
-    if( nil != firstResult && nil != resultCount) 
+    if( nil != firstResult && nil != resultCount)
       firstResult = 0  if(!(firstResult.is_a? Integer))
       firstResult = 0  if(firstResult < 0)
     end
@@ -339,7 +404,7 @@ class Library
     if("like" == comparison)
       value = '%' + value + '%';
     end
-    return request(fieldsSelection, value, field, orderBy, firstResult, resultCount); 
+    return request(fieldsSelection, value, field, orderBy, firstResult, resultCount);
   end
 
   def request(fieldsSelection, value, field, orderBy, firstResult, resultCount)
@@ -355,7 +420,7 @@ class Library
 
     if(firstResult && resultCount)
       request << "LIMIT #{firstResult},#{resultCount}";
-    else 
+    else
       if(resultCount)
            request << "LIMIT #{resultCount}";
       end
