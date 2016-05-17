369c9f2f158d7fa30ce61b1a0a9e7c9ebe7e61d0
Camille Masson <beuiot@gmail.com>
2016-05-17 21:33:04 +0200

    Added basic version information (milestone name and url)

---
+++ html_src/js/version.js
@@ -0,0 +1,6 @@
+/* jshint unused: false */
+
+var gJukeboxVersion = {
+	milestoneName: 'Extra Large Donkey Balls',
+	milestoneUrl: 'https://github.com/nestorInc/jukebox/issues?q=milestone%3A%22Extra+large+donkey+balls%22'
+};
